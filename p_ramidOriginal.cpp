ba7bf19865a565963686c54bac02994e459e8563
Xavier P <44008221+Xavosorcerer@users.noreply.github.com>
2019-02-19 19:22:18 -0500

    Line 7 P
    Changed default letter to P because im dum

--- p_ramidOriginal.cpp
+++ p_ramidOriginal.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main (){
   int repeats = 60;
-  char letter = 'k';
+  char letter = 'p';
   for (int i = 1; i <= repeats; i++){
     for (int n = 1; n <= i; n++) {
       std:cout << letter;
